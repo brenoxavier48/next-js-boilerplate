a57fb49b05e67649fdbc7ae08182778a2b7b9c7b
Breno Xavier <brenoxavier8.bx@gmail.com>
2022-03-09 22:30:32 -0300

    feat: add some styled components

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Logo: imagem de um átomo, escrito 'React Avançado' ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, React, NextJs and Styled Components
+    </S.Description>
+    <S.Logo
+      src="/img/hero-illustration.svg"
+      alt="Pessoa de costas sentada em uma cadeira, usando o computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
